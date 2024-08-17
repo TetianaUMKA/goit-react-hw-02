1a9896456ad27a0b275dcc9ded2c3c831c143b46
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2024-08-17 22:40:39 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./components/App.jsx";
+
+import "modern-normalize";
+
+import App from "./components/App/App";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
