f0e5f308030842c2723eb109fb3a84877489aa22
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2024-08-17 22:47:22 +0300

    Create Options.jsx

---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,10 @@
+export default function Options() {
+  return (
+    <div>
+      <button>Good 🙂</button>
+      <button>Neutral 😐</button>
+      <button>Bad 🙃</button>
+      <button>Reset 🔄</button>
+    </div>
+  );
+}
