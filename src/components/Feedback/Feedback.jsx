c31ffb476b9c8113a17dba993ac6b3cf94a51519
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2024-08-19 19:13:07 +0300

    Update Feedback.jsx

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,3 +1,25 @@
-export default function Feedback({ props }) {
-  return <div>{props ? <div></div> : <p>No feedback yet</p>}</div>;
+export default function Feedback({
+  feedback: { good, neutral, bad },
+  total,
+  percentage,
+}) {
+  return (
+    <ul>
+      <li>
+        <p>Good: {good}</p>
+      </li>
+      <li>
+        <p>Neutral: {neutral}</p>
+      </li>
+      <li>
+        <p>Bad: {bad}</p>
+      </li>
+      <li>
+        <p>Total: {total}</p>
+      </li>
+      <li>
+        <p>Positive: {percentage}%</p>
+      </li>
+    </ul>
+  );
 }
