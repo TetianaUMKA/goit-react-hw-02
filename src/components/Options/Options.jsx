da9400d68b09a2cce84aa1088e91cdbd9f1c50fb
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2024-08-19 19:41:19 +0300

    Update Options.jsx

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -2,13 +2,14 @@ export default function Options({
   feedbackTypes: [good, neutral, bad],
   onUpdateFeedback,
   total,
+  onResetFeedback,
 }) {
   return (
     <div>
       <button onClick={() => onUpdateFeedback(good)}>Good 🙂</button>
       <button onClick={() => onUpdateFeedback(neutral)}>Neutral 😐</button>
       <button onClick={() => onUpdateFeedback(bad)}>Bad 🙃</button>
-      {total > 0 && <button>Reset 🔄</button>}
+      {total > 0 && <button onClick={() => onResetFeedback()}>Reset 🔄</button>}
     </div>
   );
 }
